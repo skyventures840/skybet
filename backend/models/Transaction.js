f44626d3bf9fef45ad21bd07a433b9be81dfd5f8
skyventures840 <skyventures840@gmail.com>
2025-10-12 20:09:47 +0300

    Add files via upload

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -25,7 +25,7 @@ const transactionSchema = new mongoose.Schema({
   },
   currency: {
     type: String,
-    enum: ['USD', 'BTC', 'ETH', 'LTC', 'USDT', 'BNB'],
+    trim: true,
     default: 'USD'
   },
   walletAddress: {
