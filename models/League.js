02dc2af0a580d4d69548ea16379bafec3ec9d574
skyventures840 <skyventures840@gmail.com>
2025-12-21 12:53:57 +0300

    Delete models directory

--- models/League.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const leagueSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  leagueId: { type: String, required: true, unique: true },
-  externalPrefix: { type: String, required: true }
-});
-
-module.exports = mongoose.model('League', leagueSchema); 
